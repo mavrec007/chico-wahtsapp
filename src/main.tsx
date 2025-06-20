8b300145b376c102016c8abd7b95dae84d2c8358
root <root@DESKTOP-KVH5ITH.localdomain>
2025-06-21 01:48:51 +0300

    upgrade

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,4 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './lib/i18n.ts';
-import '@/services/whatsapp/whatsapp.client';
 createRoot(document.getElementById("root")!).render(<App />);
